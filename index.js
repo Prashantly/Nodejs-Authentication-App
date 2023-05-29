6c6d4a8925ea19e7016265206f1ef00459f025ed
Prashant <prashantyallatti79968@gmail.com>
2023-05-29 17:05:42 +0530

    setting up ejs view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 8000;
 
+//setup view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 //user express router
 app.use("/", require("./routes/index"));
 
