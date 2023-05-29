448b1d04ba7f51bfc87c55110366a94d0d75e507
Prashant <prashantyallatti79968@gmail.com>
2023-05-29 17:19:25 +0530

    set up layots and partials

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
 const port = 8000;
+
+app.use(expressLayouts);
 
 //setup view engine
 app.set("view engine", "ejs");
