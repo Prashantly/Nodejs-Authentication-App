bdc64c85a83739ef9452d1835c8d910ae727b138
Prashant <prashantyallatti79968@gmail.com>
2023-05-30 18:31:59 +0530

    satic files for pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,1 @@
-module.exports.profile = function (req, res) {
-  return res.end("<h1>User profile</h1>");
-};
+
--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const app = express();
 
 const port = 8000;
 
-app.use(expressLayouts);
 app.use(express.static("./assets"));
+app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //setup view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 //user express router
-app.use("/", require("./routes/index"));
+app.use("/", require("./routes"));
 
 app.listen(port, (err) => {
   if (err) {
