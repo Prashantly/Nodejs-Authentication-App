d5fdcbe6e6af5071729cc39bd594a23bf7da0824
Prashant <prashantyallatti79968@gmail.com>
2023-05-30 20:14:49 +0530

    rendering signUp and signIn pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,1 +1,11 @@
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Auth app | Sign Up",
+  });
+};
 
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Auth app | Sign In",
+  });
+};
